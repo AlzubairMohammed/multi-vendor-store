1a0b797d9663c82b857d0ec894aaee3d816cc86e
AlzubairMohammed <dev.alzubair@gmail.com>
2023-10-01 17:39:13 +0300

    Add files for error handler and async handler

--- utils/errorResponse.js
+++
@@ -1,9 +0,0 @@
-class ErrorResponse extends Error {
-    constructor(message, stateCode) {
-      super(message);
-      this.stateCode = stateCode;
-    }
-  }
-  
-  module.exports = ErrorResponse;
-  
