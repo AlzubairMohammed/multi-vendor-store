06ad210dd5446389d5589b55bfdbe3cb126ad777
AlzubairMohammed <dev.alzubair@gmail.com>
2023-11-06 22:41:32 +0300

    Add subcategories model

--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class SubCategory extends Model {
     /**
@@ -13,13 +11,16 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  SubCategory.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING,
-    category_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'SubCategory',
-  });
+  SubCategory.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.STRING,
+      category_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "SubCategory",
+    }
+  );
   return SubCategory;
 };
