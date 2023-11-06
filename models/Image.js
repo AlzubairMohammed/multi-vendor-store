37a3eb413200f826340ff33a210142e632499cc5
AlzubairMohammed <dev.alzubair@gmail.com>
2023-11-06 23:38:52 +0300

    Edit Image model

--- models/Image.js
+++ models/Image.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Image extends Model {
     /**
@@ -13,12 +11,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Image.init({
-    image: DataTypes.STRING,
-    product_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Image',
-  });
+  Image.init(
+    {
+      image: DataTypes.STRING,
+      product_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Image",
+    }
+  );
   return Image;
 };
