2e86fdaf328fd39b1065429f3b638ba93d4b36c3
AlzubairMohammed <dev.alzubair@gmail.com>
2023-12-10 23:22:56 +0300

    delete subcategory model'

--- models/User.js
+++ models/User.js
@@ -1,87 +1,90 @@
-const config = require('../config/env');
-const Sequelize = require('sequelize');
+const config = require("../config/env");
+const Sequelize = require("sequelize");
 const Model = Sequelize.Model;
 
-class User extends Model {};
-User.init({
+class User extends Model {}
+User.init(
+  {
     id: {
-        type: Sequelize.BOOLEAN,        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      country: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      image: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      tel: {
-        type: Sequelize.STRING(13),
-        allowNull: false,
-      },
-      address: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      role: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      c: {
-        type: Sequelize.BOOLEAN(),
-        allowNull: false,
-        defaultValue: false,
-      },
-      u: {
-        type: Sequelize.BOOLEAN(),
-        allowNull: false,
-        defaultValue: false,
-      },
-      d: {
-        type: Sequelize.BOOLEAN(),
-        allowNull: false,
-        defaultValue: false,
-      },
-      created: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updated: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-},
-{
-    sequelize:config,
-    modelName:'User',
-    tableName:'users',
-    timestamps:false
-});
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    country: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    image: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    tel: {
+      type: Sequelize.STRING(13),
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    role: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    c: {
+      type: Sequelize.BOOLEAN(),
+      allowNull: false,
+      defaultValue: false,
+    },
+    u: {
+      type: Sequelize.BOOLEAN(),
+      allowNull: false,
+      defaultValue: false,
+    },
+    d: {
+      type: Sequelize.BOOLEAN(),
+      allowNull: false,
+      defaultValue: false,
+    },
+    created: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updated: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  },
+  {
+    sequelize: config,
+    modelName: "User",
+    tableName: "users",
+    timestamps: false,
+  }
+);
 
-User.associate = ({Product})=>{
-    User.hasMany(Product, {
-        foreignKey:'user_id',
-        as:'products',
-        onDelete:'CASCADE',
-        onUpdate:'CASCADE'
-    });
-}
+User.associate = ({ Product }) => {
+  User.hasMany(Product, {
+    foreignKey: "user_id",
+    as: "products",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  });
+};
 
 module.exports = User;
