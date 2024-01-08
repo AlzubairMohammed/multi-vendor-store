5d4186eff6a09e453c803dd9fc206b8f0ff92d56
AlzubairMohammed <dev.alzubair@gmail.com>
2024-01-09 00:55:09 +0300

    Enance updateCategory and deleteCategory

--- controllers/categories.js
+++ controllers/categories.js
@@ -52,39 +52,49 @@ exports.createCategory = async (req, res, next) => {
   return res.json({ status: httpStatus.SUCCESS, data });
 };
 // Update category
-exports.updateCategory = async (req, res, next) => {
+exports.updateCategory = asyncWrapper(async (req, res, next) => {
+  const errors = validationResult(req);
   const id = req.params.id;
-  const { name } = req.body;
   const { files } = req;
-  let fileName = "";
-  let category = await Category.findOne({ where: { id } });
-  if (category)
-    Object.keys(files).forEach((key) => {
-      fileName = Date.now() + files[key].name + "";
-      const filepath = path.join(__dirname, "../uploads", fileName);
-      files[key].mv(filepath, (err) => {
-        if (err) return res.status(500).json({ status: "error", message: err });
-      });
+  if (!errors.isEmpty()) {
+    const error = ErrorResponse.create(errors.array(), 400, httpStatus.FAIL);
+    return next(error);
+  }
+  Object.keys(files).forEach((key) => {
+    fileName = Date.now() + files[key].name + "";
+    const filepath = path.join(__dirname, "../uploads", fileName);
+    files[key].mv(filepath, (err) => {
+      if (err) {
+        const error = ErrorResponse.create(err, 400, httpStatus.FAIL);
+        return next(error);
+      }
     });
-  try {
-    // Delete old image form uploads directory
-    fs.unlinkSync(path.join(__dirname, `../uploads/${category.image}`));
-  } catch (err) {
-    return next(new ErrorResponse("old image not found", 404));
+  });
+  req.body.image = fileName;
+  const [data] = await Category.update(req.body, {
+    where: { id },
+  });
+  if (!data) {
+    const error = ErrorResponse.create(
+      `Category with id = ${id} is not found`,
+      404,
+      httpStatus.FAIL
+    );
+    return next(error);
   }
-  Category.update({ name, image: fileName }, { where: { id } });
-  res.status(200).json({ msg: `category updated` });
-};
-// Delete category
-exports.deleteCategory = async (req, res, next) => {
+  res.json({ status: httpStatus.SUCCESS, data: "Category updated" });
+});
+
+exports.deleteCategory = asyncWrapper(async (req, res, next) => {
   const id = req.params.id;
-  let category = await Category.findOne({ where: { id } });
-  if (category) Category.destroy({ where: { id } });
-  try {
-    // Delete old image form uploads directory
-    fs.unlinkSync(path.join(__dirname, `../uploads/${category.image}`));
-  } catch (err) {
-    return next(new ErrorResponse("old image not found", 404));
+  const data = await Category.destroy({ where: { id } });
+  if (!data) {
+    const error = ErrorResponse.create(
+      `Category with id = ${id} is not found`,
+      404,
+      httpStatus.FAIL
+    );
+    return next(error);
   }
-  res.status(200).json({ msg: `category deleted` });
-};
+  return res.json({ status: httpStatus.SUCCESS, data: "Category deleted" });
+});
