3fe93bea762cc5694cd08ce0fe3454d75686f763
AlzubairMohammed <dev.alzubair@gmail.com>
2024-01-15 21:03:05 +0300

    Solve categories problem

--- migrations/20231031133935-create-sub-category.js
+++ migrations/20231031133935-create-sub-category.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Categories",
+          model: "categories",
           key: "id",
         },
         onUpdate: "CASCADE",
